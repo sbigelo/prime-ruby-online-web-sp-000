f088a8778508859e1dbfd5879afffdbbd441a113
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-08 20:11:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,6 @@
 def prime?(n)
- if n <= 1
-        false
-    elsif n == 2
-        true
-    else 
-        (2..n/2).none? { |i| n % i == 0}
-    end
+return false if n < 2
+
+  (2..n/2).none?{|i| n % i == 0}
 end
  
